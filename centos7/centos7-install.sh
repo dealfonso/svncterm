ea574de029ffc7a40021efa475415e92841f29f1
Alfonso <alpegon3@upvnet.upv.es>
2016-03-11 14:34:13 +0100

    Added centos7 install scripts for the svncterm bin  - The standalone can be executed using: curl URL-TO-RAW-SCRIPT | sh -  - The normal install need the repository to be downloaded first

---
+++ centos7/centos7-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+yum install git wget zlib-devel libvncserver-devel gnutls-devel libjpeg-devel -y
+wget https://raw.githubusercontent.com/proxmox/vncterm/master/glyphs.h
+cp ../svncterm.c .
+cp ../svncterm.h .
+gcc -O2 -g -o svncterm svncterm.c -Wall -Wno-deprecated-declarations -D_GNU_SOURCE -lvncserver -lnsl -lpthread -lz -ljpeg -lutil -lgnutls
+install -s -m 0755 svncterm /usr/bin/
